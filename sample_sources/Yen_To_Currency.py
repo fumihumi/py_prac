dca74d7f6b1984251a402190623ec410f2ee9f95
fumihumi <bgts1260011@gn.iwasaki.ac.jp>
2017-12-26 22:27:57 +0900

    fix directory/file

--- sample_sources/Yen_To_Currency.py
+++ sample_sources/Yen_To_Currency.py
@@ -8,14 +8,15 @@ class YenToCurrency:
 #2. 1ドル=100円, 1ユーロ=120円で換算してください。
     def doll(self):
         #ここを埋めてください。
-        return  f"{self.yen / 100}ドルです。"
+        return  f"{self.yen / 100}ドル"
 
 
     def euro(self):
         #ここを埋めてください。
-        return f"{self.yen * 1.2}ユーロです。"
+        return f"{self.yen * 1.2}ユーロ"
 
 #正しくできているか確認するために1200円をドル・ユーロに換算して例のように出力してください。
 exchange = YenToCurrency(1200)
 
-print(f"{exchange.yen}円は{exchange.doll()}")
+print(f"{exchange.yen}円は{exchange.doll()}です。")
+print(f'{exchange.yen}円は→{exchange.euro()}です。')
